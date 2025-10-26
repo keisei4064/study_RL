dbdc1fb2886b21129fae7203029494c6f20c07f7
Keisei Tsubomoto <keisei20021415@gmail.com>
2025-10-26 12:37:48 +0900

    4.1まで

---
+++ deep-learning-from-scratch-4/ch04/iterative_policy_evaluation.py
@@ -0,0 +1,41 @@
+# 反復方策評価 (上書き方式)
+
+import matplotlib.pyplot as plt
+
+V = {"L1": 0.0, "L2": 0.0}
+V_history = [V.copy()]
+
+cnt = 0
+while True:
+    # V_(k+1) = Σ{π(a|s) * 1 * (R + γ * V_k)}
+    temp = 0.5 * (-1 + 0.9 * V["L1"]) + 0.5 * (1 + 0.9 * V["L2"])
+    delta_L1 = abs(temp - V["L1"])
+    V["L1"] = temp
+    
+    temp = 0.5 * (0 + 0.9 * V["L1"]) + 0.5 * (-1 + 0.9 * V["L2"])
+    delta_L2 = abs(temp - V["L2"])
+    V["L2"] = temp
+
+    # 小数第4位までにフォーマット
+    formatted_V = {k: f"{v:.4f}" for k, v in V.items()}
+    print(f"cnt: {cnt} V: {formatted_V}")
+
+    # 収束判定
+    delta = max(delta_L1, delta_L2)  # 更新量最大値
+    cnt += 1
+    if delta < 0.0001:
+        break
+
+    V_history.append(V.copy())
+
+
+print(f"cnt: {cnt} V: {formatted_V}")
+
+# 結果の可視化
+plt.plot(range(len(V_history)), [v["L1"] for v in V_history], label="L1")
+plt.plot(range(len(V_history)), [v["L2"] for v in V_history], label="L2")
+plt.xlabel("cnt")
+plt.ylabel("V")
+plt.legend()
+plt.grid()
+plt.show()
