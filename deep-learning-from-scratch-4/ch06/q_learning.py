cda1d8055794b3014b0e85285fb4091581eb23a5
Keisei Tsubomoto <keisei20021415@gmail.com>
2025-11-01 19:42:55 +0900

    6章終わり

--- deep-learning-from-scratch-4/ch06/q_learning.py
+++ deep-learning-from-scratch-4/ch06/q_learning.py
@@ -7,6 +7,7 @@ sys.path.append(
 from collections import defaultdict
 import numpy as np
 from common.gridworld import GridWorld
+from common.utils import greedy_probs
 from common.save_animation import save_animation_with_steps
 from matplotlib.figure import Figure
 import pathlib
@@ -16,45 +17,46 @@ class QLearningAgent:
     def __init__(self):
         self.gamma = 0.9
         self.alpha = 0.8
-        self.epsilon = 0.2
-        # self.epsilon = 0.1
+        self.epsilon = 0.1
         self.action_size = 4
+
+        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
+        self.pi = defaultdict(lambda: random_actions)  # ターゲット方策
+        self.b = defaultdict(lambda: random_actions)  # 挙動方策
         self.Q = defaultdict(lambda: 0)
 
     def get_action(self, state):
-        # 明示的に分布を持たない
-        
-        if np.random.rand() < self.epsilon:
-            # random (ε)
-            return np.random.choice(self.action_size)  
-        else:
-            # greedy
-            qs = [self.Q[state, a] for a in range(self.action_size)]
-            # 単に現在のsについて，Q(s,a)が最大となるaを返す
-            return np.argmax(qs) 
+        action_probs = self.b[state]
+        actions = list(action_probs.keys())
+        probs = list(action_probs.values())
+        return np.random.choice(actions, p=probs)  # a ~ π(a|s)
 
     def update(self, state, action, reward, next_state, done):
         if done:
-            next_q_max = 0 
+            next_q_max = 0  # ゴール地点の価値は0
         else:
             next_qs = [self.Q[next_state, a] for a in range(self.action_size)]
-            next_q_max = max(next_qs)
+            next_q_max = max(next_qs)  # 次の状態での最大価値
 
         target = reward + self.gamma * next_q_max
         self.Q[state, action] += (target - self.Q[state, action]) * self.alpha
-        
-        # 方策の確率分布更新は要らない
+
+        self.pi[state] = greedy_probs(
+            self.Q, state, epsilon=0
+        )  # greedyなターゲット方策πを更新
+        self.b[state] = greedy_probs(
+            self.Q, state, self.epsilon
+        )  # ε-greedyな挙動方策bを更新
 
 
 env = GridWorld()
 agent = QLearningAgent()
-episodes = 1000
+episodes = 10000
 
 
 figs: list[Figure] = []
 frame_names: list[str] = []
-save_rate = 50
-
+save_rate = 500
 
 for episode in range(episodes):
     state = env.reset()
@@ -74,7 +76,8 @@ for episode in range(episodes):
         figs.append(fig)
         frame_names.append(f"episode={episode}")
 
-_, pi_fig = env.render_q(agent.Q)
+
+env.render_q(agent.Q)
 
 
 # 行動価値関数Q(s,a)更新のアニメーション
